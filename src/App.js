d81c27b3cc1b5c1d016ffcbf1b28be95c0ea9b9e
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2021-05-10 16:57:37 -0300

    Finishing the card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import CardPokemon from "./components/Card/index.js";
 
 function App() {
-  return (
-    <CardPokemon
-      name="batatat"
-      image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-    />
-  );
+  return <h1>Hello World!</h1>;
 }
 
 export default App;
